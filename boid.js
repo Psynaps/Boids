4041d06ce1a771eb03ca7b3ee1862c9b0f2de768
Michael Gunn <mgunn@ucsc.edu>
2020-03-23 23:01:21 -0700

    Allignment working

--- boid.js
+++ boid.js
@@ -1,8 +1,10 @@
 class Boid {
     constructor() {
-        this.position = createVector(width / 2, height / 2);
+        this.position = createVector(random(width), random(height));
         this.velocity = p5.Vector.random2D();
+        this.velocity.setMag(random(2, maxSpeed));
         this.acceleration = createVector(); //can change to add in acceleration later
+        this.maxForce = 1; //provides an upper bound to the change of direction a boid can make in an instant. This creates more realistic movement
 
     }
 
@@ -22,10 +24,36 @@ class Boid {
         }
     }
 
+    flock(boids) {
+        let allignment = this.allign(boids);
+        allignment.mult(allignScale); //allow for slider control of grouping factors
+        this.acceleration.add(allignment);
+    }
+
+    allign(boids) {
+        let steer = createVector();
+        let total = 0;
+        for (let boid of boids) { //JS for each
+            let d = dist(this.position.x, this.position.y, boid.position.x, boid.position.y); //d = dist(this, boid)
+            if (boid != this && d < perceptionRadius) {
+                steer.add(boid.velocity);
+                total++;
+            }
+        }
+        if (total > 0) { //prevents divide by 0
+            steer.div(total);
+            steer.setMag(maxSpeed);
+            steer.sub(this.velocity);
+            steer.setMag(this.maxForce); //Limit the ammount of force imparted by allignment affect
+        }
+        return steer;
+    }
+
     update() {
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
-        this.acceleration.setMag(0);
+        this.velocity.limit(this.maxSpeed); // set speed to max(speed, maxSpeed)
+        this.acceleration.setMag(0); //allows for acceleration to be used as the delta v from the forces, but then is reset for the next tick
     }
 
 
--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 const flock = [];
 
+let allignScale, cohesionScale, separationScale;
+let maxSpeed;
+let perceptionRadius; //add in feature later to highlight the perception radius of a single boid (first in the flock) to see how it perceives
+
 function setup() {
     createCanvas(640, 360);
-    for (let i = 0; i < 10; i++) {
+    allignScale = cohesionScale = separationScale = 1;
+    maxSpeed = 4;
+    perceptionRadius = 50;
+    for (let i = 0; i < 100; i++) {
         flock.push(new Boid());
     }
 }
@@ -12,6 +19,7 @@ function draw() {
 
     for (let boid of flock) {
         boid.walls();
+        boid.flock(flock);
         boid.update();
         boid.show();
     }
